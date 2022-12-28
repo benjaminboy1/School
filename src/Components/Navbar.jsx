29330c6926dc530aa92ace8e9a0ab75bb6350464
Benjamin Mumbita <benjaminmumbita10@gmail.com>
2022-12-28 02:54:50 +0100

    firth commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import benja from "../Assets/Images/benja.jpg"
+
+
 
 const Navbar = () => {
     const menuLinks = [
@@ -10,13 +13,20 @@ const Navbar = () => {
 
     ];
 
+    const social_media = [
+    
+        "logo-linkedin",
+        "logo-twitter",
+        "logo-facebook"
+    ]
+
 
     return(
-        <nav>
-            <div className="flex items-center justify-around">
+        <nav className="bg-gradient-to-r from-cyan-700 via-teal-500 to-slate-900">
+            <div className="flex items-center justify-between">
                 
                 <div className="mx-7">
-                    <h4 className="text-cyan-500 text-2xl font-bold">
+                    <h4 className="justify-start text-cyan-500 text-2xl font-bold">
                       BenmSchool
                     </h4>
                 </div>
@@ -24,8 +34,8 @@ const Navbar = () => {
                     <ul className="flex items-center py-2 gap-6 text-lg"> 
                         {
                             menuLinks?.map((menu,i)=>(
-                                <li key={i} className="hover:text-cyan-500">
-                                <a href={menu?.link}>
+                                <li key={i} className="text-white hover:text-cyan-500">
+                                <a href={menu?.link} className="">
                                     {menu?.name}
                                 </a>
                                 </li>
@@ -33,8 +43,29 @@ const Navbar = () => {
                         }
                     </ul>
                 </div>
-                <div>
-                        <input type="text" placeholder="search" />
+                
+                <div className="flex items-center justify-between">
+                <div className="flex items-center bg-white  py-1 rounded-lg">
+                        <button className="p-1 "><ion-icon name="search-outline"></ion-icon></button>
+                        <input type="text" placeholder="search" className="border-none"/>
+                        <div className="">
+                        <button>Students<ion-icon name="caret-down-circle-outline"></ion-icon></button>
+                        </div> 
+                </div>
+                <div className="flex items-center text-white ">
+                    {
+                        social_media?.map(icon=>(
+                            <div key={icon} className="flex items-center p-2 justify-between">
+                            <ion-icon name={icon} ></ion-icon>
+                        </div>
+                        ))
+                       
+                    }
+                    <div className="border-white border-2 rounded-full">
+                        <img src={benja} alt="" className="w-8 h-8 rounded-full"/>
+                    </div>
+                
+                </div>
                 </div>
             </div>
 
