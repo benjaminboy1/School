6f413a15f9a413d834b94af1768b9823bb30fd49
Benjamin Mumbita <benjaminmumbita10@gmail.com>
2022-12-31 02:29:43 +0100

    responsive one

--- src/Components/DropDownProfile.jsx
+++ src/Components/DropDownProfile.jsx
@@ -5,10 +5,10 @@ const DropDownProfile = () =>{
 
         <div className="flex flex-col dropDownProfile">
             <ul className="flex flex-col gap-4 text-center text-cyan-700">
-            <li className=""><ion-icon name="person-circle-sharp" className=""/>  Profile</li>
-                <li><ion-icon name="settings-sharp"></ion-icon> Settings</li>
-                <div className="border border-gray-400 rounded"></div>
-                <li><ion-icon name="log-out-sharp"></ion-icon> Logout</li>
+            <li className="hover:text-gray-600 hover:animate-bounce"><ion-icon name="person-circle-sharp" />  Profile</li>
+                <li className="hover:text-gray-600 hover:animate-bounce"><ion-icon name="settings-sharp" className=""></ion-icon> Settings</li>
+                <div className="border border-gray-400 rounded" ></div>
+                <li className="hover:text-gray-600 hover:animate-bounce"><ion-icon name="log-out-sharp"></ion-icon> Logout</li>
             </ul>
         </div>
     )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,10 +9,10 @@ import DropDownProfile from "./DropDownProfile";
 const Navbar = () => {
 
     const menuLinks = [
-        { name: 'HOME',link: "#home"},
-        { name: 'REGISTER', link: "#register"},
-        { name: 'PAYEMENT', link: "#payement"},
-        { name: 'REPORTER', link: "#reporter"},
+        { name: 'Home',link: "#home"},
+        { name: 'Register', link: "#register"},
+        { name: 'Payement', link: "#payement"},
+        { name: 'Reporter', link: "#reporter"},
        
 
     ];
@@ -52,18 +52,19 @@ const Navbar = () => {
 
     return(
         <nav className="bg-gradient-to-r from-cyan-700 via-teal-500 to-slate-900">
-            <div className="flex items-center justify-between">
+            <div className="flex container items-center justify-between mx-auto">
                 
                 <div className="mx-7">
-                    <h4 className="justify-start text-cyan-500 text-2xl font-bold">
+                    <h4 className="justify-start text-cyan-100 text-1xl ">
                       BenmSchool
                     </h4>
                 </div>
-                <div>
-                    <ul className="flex items-center py-2 gap-6 text-lg"> 
+                
+                <div className=" md:block hidden ">
+                    <ul className="flex items-center py-2 gap-8"> 
                         {
                             menuLinks?.map((menu,i)=>(
-                                <li key={i} className="text-white hover:text-cyan-300">
+                                <li key={i} className="text-white hover:text-cyan-300 ">
                                 <a href={menu?.link} className="">
                                     {menu?.name}
                                 </a>
@@ -73,44 +74,49 @@ const Navbar = () => {
                     </ul>
                 </div>
                 
-                <div className="flex items-center justify-between">
-                <div className="flex items-center bg-white rounded-lg">
+                <div className="flex items-center justify-between ">
+                <div className="flex items-center bg-white rounded-lg ">
 
                 <label class="relative block">
                         <span class="absolute inset-y-0 left-0 flex items-center pl-2">
                         <ion-icon name="search-sharp"></ion-icon>
                         </span>
-                    <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:ring-none  sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
+                    <input class="placeholder:italic placeholder:text-slate-400 block bg-white w-full rounded-md py-1 pl-9 pr-2 shadow-sm focus:outline-none focus:ring-none  sm:text-sx" placeholder="Search..." type="text" name="search"/>
                 </label>
 
                        <div className="">
-                        <button className="text-gray-700">Students<ion-icon name="caret-down-circle-outline"></ion-icon></button>
+                        <button className="md:block hidden text-gray-700 right-1 text-xs">Students</button>
                         </div> 
                 </div>
-                <div className="flex flex justify-around  items-center text-white ">
+                <div className="flex  text-white  ">
                     {
                         social_media?.map(icon=>(
-                            <div key={icon} className="flex items-center p-2 justify-between">
+                            <div key={icon} className="flex items-center p-2 md:block hidden">
                             <ion-icon name={icon} ></ion-icon>
                         </div>
                         ))
                        
                     }
-                    <button onClick={handleThemeSwitch} className="left-px">
+                    <button onClick={handleThemeSwitch} className="p-2 md:block hidden">
                         {theme === 'light' ?  <BsMoonStarsFill/> : <BsFillSunFill/>}
                     </button>
-                    <div className="border-white border-2 rounded-full cursor-pointer" onClick={() => setOpenProfile
+                
+                </div>
+           
+                
+                </div>
+                <div className="md:block hidden border-white border-2 rounded-full cursor-pointer profili" onClick={() => setOpenProfile
                         ((prev) => !prev)}>
-                        <img src={benja} alt="" className="w-8 h-8 rounded-full"/>
                         {
                             openProfile && <DropDownProfile />
                         }
+                        <img src={benja} alt="" className="w-7 h-7 rounded-full "/>
+                        
                     </div>
-                
-                </div>
-                </div>
-            </div>
 
+               
+            </div>
+           
         </nav>
     )
 }
